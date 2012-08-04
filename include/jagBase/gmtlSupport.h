4385f6993af4ab900febbc927ec153d820ef49e2
pmartz <pmartz@Skew>
2012-08-04 08:42:49 -0600

    Using new GMTL functionality.

--- include/jagBase/gmtlSupport.h
+++ include/jagBase/gmtlSupport.h
@@ -8,25 +8,9 @@
 namespace gmtl {
 
 
-template <typename T>
-inline Matrix<T, 4,4>& setLookAt( Matrix<T, 4,4>& result,
-    const Point<T, 3>& eye, const Point<T, 3>& center, const Vec<T, 3>& up )
-{
-    Vec<T, 3> f( center-eye ); normalize( f );
-    Vec<T, 3> s; cross( s, f, up ); normalize( s );
-    Vec<T, 3> u; cross( u, s, f ); normalize( u );
-
-    Matrix<T, 4,4> orient;
-    zero( orient );
-    orient(0,0) = s[0]; orient(0,1) = s[1]; orient(0,2) = s[2];
-    orient(1,0) = u[0]; orient(1,1) = u[1]; orient(1,2) = u[2];
-    orient(2,0) = -f[0]; orient(2,1) = -f[1]; orient(2,2) = -f[2];
-    orient(3,3) = T(1.);
-
-    result = orient * makeTrans< Matrix<T, 4,4> >( -eye );
-    result.mState = Matrix<T, 4,4>::AFFINE;
-    return( result );
-}
+    // This file should be used as a placeholder for any additional GMTL
+    // functionality needed by JAG, but not yet added to GMTL proper.
+    // This file is currently unused.
 
 
 // gmtl
