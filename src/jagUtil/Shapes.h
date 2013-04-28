5d40f7f297a28535b469b47d5b3d7b07c162ed43
pmartz <pmartz@Utah>
2013-04-28 15:09:31 -0600

    Adding empty jagUtil library.

---
+++ src/jagUtil/Shapes.h
@@ -0,0 +1,42 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+*
+* jag3d is (C) Copyright 2011-2012 by Kenneth Mark Bryden and Paul Martz
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License version 2.1 as published by the Free Software Foundation.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the
+* Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+* Boston, MA 02111-1307, USA.
+*
+*************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef __JAGUTIL_SHAPES_H__
+#define __JAGUTIL_SHAPES_H__ 1
+
+#include <jagUtil/Export.h>
+#include <jagDraw/Drawable.h>
+#include <gmtl/gmtl.h>
+
+
+namespace jagUtil
+{
+
+
+JAGUTIL_EXPORT jagDraw::Drawable* makePlane( const gmtl::Point3f& corner, const gmtl::Vec3f& u, const gmtl::Vec3f& v,
+    const short subU=1, const short subV=1, jagDraw::Drawable* drawable=NULL );
+
+
+// jagUtil
+}
+
+
+// __JAGUTIL_SHAPES_H__
+#endif
