8f488d76d5e3dd7de8df9c6f464205570ce6016a
pmartz <pmartz@localhost>
2013-07-22 15:55:24 -0600

    Buffer aggregation dev checkpoint. Correctly offsets element indices without aggregating element buffers. Currently, the code is disabled while I look into a crash related to querying bounding volumes after the visitor runs.

--- src/jagUtil/BufferAggregationVisitor.cpp
+++ src/jagUtil/BufferAggregationVisitor.cpp
@@ -20,6 +20,7 @@
 
 #include <jagUtil/BufferAggregationVisitor.h>
 #include <jagDraw/Drawable.h>
+#include <jagDraw/DrawCommand.h>
 #include <jagSG/Node.h>
 
 #include <boost/foreach.hpp>
@@ -44,7 +45,7 @@ BufferAggregationVisitor::BufferAggregationVisitor( jagSG::NodePtr node, const s
     // This only resets the root node bounds. We need to reset bounds
     // on the entire scene graph.
     JAG3D_NOTICE( "Need a ResetBoundsVisitor." );
-    node->resetBounds();
+    //node->resetBounds();
 
     if( _vaop->getVertexArrayCommandList().empty() )
     {
@@ -52,6 +53,7 @@ BufferAggregationVisitor::BufferAggregationVisitor( jagSG::NodePtr node, const s
         return;
     }
 
+    // Store the aggregated VAO in the root node's CommandMap.
     jagDraw::CommandMapPtr commands( node->getCommandMap() );
     if( commands == NULL )
     {
@@ -60,6 +62,9 @@ BufferAggregationVisitor::BufferAggregationVisitor( jagSG::NodePtr node, const s
     }
     commands->insert( _vaop );
 
+    JAG3D_CRITICAL( "Finished." );
+#if 0
+    // Debug.
     JAG3D_DEBUG( "Final VAO/offset map:" );
     BOOST_FOREACH( const OffsetMap::value_type& data, _offsetMap )
     {
@@ -67,6 +72,7 @@ BufferAggregationVisitor::BufferAggregationVisitor( jagSG::NodePtr node, const s
         ostr << std::hex << data.first << std::dec << ", " << data.second;
         JAG3D_DEBUG( "  " + ostr.str() );
     }
+#endif
 }
 BufferAggregationVisitor::BufferAggregationVisitor( const BufferAggregationVisitor& rhs )
     : jagSG::VisitorBase( rhs )
@@ -81,6 +87,7 @@ void BufferAggregationVisitor::reset()
 {
     _vaop.reset( ( jagDraw::VertexArrayObject* )NULL );
     _nodeSet.clear();
+    _bufferObjectSet.clear();
     _offsetMap.clear();
 }
 
@@ -110,7 +117,8 @@ void BufferAggregationVisitor::visit( jagSG::Node& node )
     }
     _nodeSet.insert( &node );
 
-    pushCommandMap( node.getCommandMap() );
+    jagDraw::CommandMapPtr localCommands( node.getCommandMap() );
+    pushCommandMap( localCommands );
 
 
     jagDraw::CommandMap& commands( _commandStack.back() );
@@ -122,7 +130,14 @@ void BufferAggregationVisitor::visit( jagSG::Node& node )
         // We're going to aggregate the current VAO into the master.
         // Step 1: If the current VAO in the command stack matches the Node's
         // VAO, then remove the Node's VAO, because it will use the master
-        // from not on.
+        // from now on.
+        if( false )//localCommands != NULL )
+        {
+            jagDraw::DrawablePrepPtr& dp( (*localCommands)[ jagDraw::DrawablePrep::VertexArrayObject_t ] );
+            jagDraw::VertexArrayObjectPtr localVAO( boost::static_pointer_cast< jagDraw::VertexArrayObject >( dp ) );
+            if( localVAO == vaop )
+                localCommands->clear( jagDraw::DrawablePrep::VertexArrayObject_t );
+        }
 
         // Step 2: Combine the current VAO into the master VAO and
         // obtain the offset into the master VAO of the start of the
@@ -142,11 +157,14 @@ void BufferAggregationVisitor::visit( jagSG::Node& node )
         }
         JAG3D_INFO( ostr.str() );
 
-        // Step 3: Modify the DrawCommand indices to access the new location
-        // of the current VAO -- where it got copied to in the master VAO.
-        for( unsigned int idx=0; idx < node.getNumDrawables(); ++idx )
+        if( false )//offset > 0 )
         {
-            handleDrawable( node.getDrawable( idx ) );
+            // Step 3: Modify the DrawCommand indices to access the new location
+            // of the current VAO -- where it got copied to in the master VAO.
+            for( unsigned int idx=0; idx < node.getNumDrawables(); ++idx )
+            {
+                handleDrawable( node.getDrawable( idx ), vaop.get() );
+            }
         }
     }
 
@@ -156,8 +174,81 @@ void BufferAggregationVisitor::visit( jagSG::Node& node )
 }
 
 
-void BufferAggregationVisitor::handleDrawable( jagDraw::DrawablePtr draw )
+void BufferAggregationVisitor::offsetDrawElements( jagDraw::DrawElements* de, const size_t offset )
 {
+    jagDraw::BufferObjectPtr oldBuf( de->getElementBuffer() );
+    if( _bufferObjectSet.find( oldBuf.get() ) != _bufferObjectSet.end() )
+    {
+        // Already processed this (shared) buffer object.
+        return;
+    }
+    _bufferObjectSet.insert( oldBuf.get() );
+
+    const size_t oldByteSize( oldBuf->getBuffer()->getSize() );
+    size_t numElements( 0 );
+    switch( de->getType() )
+    {
+    case GL_UNSIGNED_BYTE: numElements = oldByteSize / sizeof( GLubyte ); break;
+    case GL_UNSIGNED_SHORT: numElements = oldByteSize / sizeof( GLushort ); break;
+    case GL_UNSIGNED_INT: numElements = oldByteSize / sizeof( GLuint ); break;
+    }
+
+    GLint* newData( (GLint*)malloc( numElements * sizeof( GLint ) ) );
+    GLint* newPtr( newData );
+
+    size_t count( numElements );
+    switch( de->getType() )
+    {
+    case GL_UNSIGNED_BYTE:
+    {
+        unsigned char* ptr( ( unsigned char* )( oldBuf->getBuffer()->getData() ) );
+        while( count-- > 0 )
+            *newPtr++ = (unsigned int)( *ptr++ + offset );
+        break;
+    }
+    case GL_UNSIGNED_SHORT:
+    {
+        unsigned short* ptr( ( unsigned short* )( oldBuf->getBuffer()->getData() ) );
+        while( count-- > 0 )
+            *newPtr++ = (unsigned int)( *ptr++ + offset );
+        break;
+    }
+    case GL_UNSIGNED_INT:
+    {
+        unsigned int* ptr( ( unsigned int* )( oldBuf->getBuffer()->getData() ) );
+        while( count-- > 0 )
+            *newPtr++ = (unsigned int)( *ptr++ + offset );
+        break;
+    }
+    }
+
+    jagBase::BufferPtr buf( new jagBase::Buffer( numElements * sizeof( GLuint ), newData ) );
+    jagDraw::BufferObjectPtr newBuf( new jagDraw::BufferObject( GL_ELEMENT_ARRAY_BUFFER, buf ) );
+    de->setElementBuffer( newBuf );
+    de->setType( GL_UNSIGNED_INT );
+
+    free( newData );
+}
+
+void BufferAggregationVisitor::handleDrawable( jagDraw::DrawablePtr draw, const jagDraw::VertexArrayObject* vaop )
+{
+    const size_t offset( _offsetMap[ const_cast<jagDraw::VertexArrayObject*>(vaop) ] );
+
+    BOOST_FOREACH( jagDraw::DrawCommandPtr& dcp, draw->getDrawCommandVec() )
+    {
+        if( dcp->getDrawCommandType() == jagDraw::DrawCommand::DrawElementsType )
+        {
+            jagDraw::DrawElements* de( static_cast< jagDraw::DrawElements* >( dcp.get() ) );
+            offsetDrawElements( de, offset );
+        }
+        else if( dcp->getDrawCommandType() == jagDraw::DrawCommand::DrawArraysType )
+        {
+            jagDraw::DrawArrays* da( static_cast< jagDraw::DrawArrays* >( dcp.get() ) );
+            da->setFirst( da->getFirst() + (GLint)( offset ) );
+        }
+        else
+            JAG3D_CRITICAL( "Unknown command" );
+    }
 }
 
 
